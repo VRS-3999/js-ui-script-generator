111f65385afb44217def3bb822f0163208f15b25
vivek <vivek>
2026-02-07 20:45:40 +0530

    post changed

--- src/pages/api/generateScript.ts
+++ src/pages/api/generateScript.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "http://localhost:8045/api";
 
 export const generateDagScript = async (payload: any) => {
   try {
